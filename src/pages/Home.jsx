13261c9b9b9388c006c832295d2792bef651a703
Rajat <rajat804@gmail.com>
2026-03-05 18:24:30 +0530

    home update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import Header from '../component/Header/Header'
 import HeroSection from '../component/HeroSection'
 import ServicesSection from '../component/ServicesSection'
+import ProjectsSection from '../component/ProjectsSection'
+import AboutSection from '../component/AboutSection'
+import LatestNewsSection from '../component/LatestNewsSection'
+import ClientsSection from '../component/ClientsSection'
+import WhyChooseUsSection from '../component/WhyChooseUsSection'
+import ContactSection from '../component/ContactSection'
+import Footer from '../component/Footer/Footer'
+import FloatingContact from '../component/FloatingContact'
 
 const Home = () => {
   return (
@@ -10,6 +18,15 @@ const Home = () => {
         <Header/> 
         <HeroSection/>
         <ServicesSection/>
+        <ProjectsSection/>
+        <AboutSection/>
+        <LatestNewsSection/>
+        <ClientsSection/>
+        <WhyChooseUsSection/>
+        <ContactSection/>
+        <Footer/>
+        <FloatingContact/>
+
     </>
   )
 }
