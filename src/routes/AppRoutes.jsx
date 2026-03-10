a5cd0cb05a370ae5765d421b71d7814783414602
Rajat <rajat804@gmail.com>
2026-03-10 12:39:31 +0530

    app routes update

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
+import Header from '../component/Header/Header'
+import Footer from '../component/Footer/Footer'
+import FloatingContact from '../component/FloatingContact'
 
 
 const AppRoutes = () => {
   return (
     <>
+        <Header/>
         <Routes>
             <Route path='/' element={<Home/>}/>
         </Routes>
+        <Footer/>
+        <FloatingContact/>
     </>
   )
 }
