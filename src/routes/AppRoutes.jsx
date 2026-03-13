085687d2be13a5084c2e6535a5a1200cf8748d07
Rajat <rajat804@gmail.com>
2026-03-13 11:53:46 +0530

    app routes update

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,6 +5,7 @@ import Header from "../component/Header/Header";
 import Footer from "../component/Footer/Footer";
 import FloatingContact from "../component/FloatingContact";
 import AboutUs from "../pages/AboutUs";
+import WebsiteDevelopment from "../pages/WebsiteDevelopment";
 
 const AppRoutes = () => {
   return (
@@ -13,6 +14,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs/>} />
+        <Route path="/website-development" element={<WebsiteDevelopment />} />
       </Routes>
       <Footer />
       <FloatingContact />
