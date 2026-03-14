0681d8735e19e0ea5b03a935dc42da5228d489f3
Rajat <rajat804@gmail.com>
2026-03-14 12:07:52 +0530

    app routes update

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -8,6 +8,8 @@ import AboutUs from "../pages/AboutUs";
 import WebsiteDevelopment from "../pages/services/WebsiteDevelopment";
 import MobileAppDevelopment from "../pages/services/MobileAppDevelopment";
 import EcommerceDevelopment from "../pages/services/EcommerceDevelopment";
+import CustomSoftwareDevelopment from "../pages/services/CustomSoftwareDevelopment";
+import ErpCrmSolutions from "../pages/services/ErpCrmSolutions";
 
 const AppRoutes = () => {
   return (
@@ -17,15 +19,23 @@ const AppRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/website-development" element={<WebsiteDevelopment />} />
-        <Route path="/mobile-app-development" element={<MobileAppDevelopment />} />
-        <Route path="/ecommerce-development" element={<EcommerceDevelopment />} />
-         {/* <Route path="/custom-software-development" element={< />} />
-         <Route path="/erp-crm-solution" element={< />} />
-       <Route path="/api-development" element={< />} />
+        <Route
+          path="/mobile-app-development"
+          element={<MobileAppDevelopment />}
+        />
+        <Route
+          path="/ecommerce-development"
+          element={<EcommerceDevelopment />}
+        />
+        <Route
+          path="/custom-software-development"
+          element={<CustomSoftwareDevelopment />}
+        />
+        <Route path="/erp-crm-solution" element={<ErpCrmSolutions />} />
+       {/* <Route path="/api-development" element={< />} />
        <Route path="/maintenance" element={< />} />
       <Route path="/software-development" element={< />} />
       <Route path="/destop-app-development" element={< />} /> */}
-        
       </Routes>
       <Footer />
       <FloatingContact />
